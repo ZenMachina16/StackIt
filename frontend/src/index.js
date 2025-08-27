4ca1f8128aed4ad09d0cc798b5aecdae9e34979c
Binayak Bhattacharjee <binayakb1604@gmail.com>
2025-08-27 12:12:18 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
-});
-
-export default api;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
