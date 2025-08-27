82217e1cd893254207b9b46fef18d7fca777933f
Binayak Bhattacharjee <binayakb1604@gmail.com>
2025-08-27 12:11:22 +0530

    Update Profile.jsx

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import api from '../index';
+//import api from '../index';
 
 const Profile = () => {
   const navigate = useNavigate();
